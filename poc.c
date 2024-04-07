b455ec6ffcc36d641028e145380dc042a78308f4
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-07 13:01:32 +0100

    poc: test for fetching data

--- poc.c
+++ poc.c
@@ -5,6 +5,8 @@
 int main(int argc, char ** argv) {
   if (argc > 1) {
     printf("hello from child stdout\n");
+    printf("how are you today?\n");
+    printf("I know I'm fine because ");
     return 0;
   }
 
@@ -15,9 +17,17 @@ int main(int argc, char ** argv) {
   args[0] = argv[0];
   args[1] = "x";
   args[2] = 0;
-  printf("trying to popen\n");
-
   int res = proc_open(args, &out, &err);
   printf("popen returned %d\n", res);
+
+  char buf[1024];
+  while (fgets(buf, sizeof(buf), out)) {
+    printf("> %s", buf);
+  }
+  printf("popen ended\n");
+
+  fclose(out);
+  fclose(err);
+
   return 0;
 }
